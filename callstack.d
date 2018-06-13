7db54ddf2f40abe546e2af816fb57cc0262d39a1
Kody A Kantor <kody@kkantor.com>
2018-06-13 19:02:45 +0000

    initial commit

---
+++ callstack.d
@@ -0,0 +1,37 @@
+/*
+ * Tries to pretty-print the callstack of a given program using the given fbt
+ * module.
+ *
+ * For example, if you want to see the ZFS calls that a 'dd' program makes you
+ * would run this:
+ *
+ *     dtrace -s callstack.d zfs '"dd"' > dd_callstack.out
+ *
+ * In another window you'd run the 'dd' program. When 'dd' finishes you'd come
+ * back to this window and Ctrl+c to stop the trace.
+ */
+
+#pragma D option quiet
+
+BEGIN
+{
+	sep = 0;
+}
+
+fbt:$1::entry
+/execname == $2/
+{
+	sep += 2;
+}
+
+fbt:$1::*
+/execname == $2/
+{
+	printf("%*s%s -> %s\n", sep, "", probefunc, probename);
+}
+
+fbt:$1::return
+/execname == $2/
+{
+	sep -= 2;
+}

d8e5a8bf3e3d3d2c9c5a50db91447a0f8ff933dc
Kody Kantor <kody@kkantor.com>
2020-04-03 14:36:29 +0000

    update ring_check.d

--- illumos/dtrace/ring_check.d
+++ illumos/dtrace/ring_check.d
@@ -6,34 +6,43 @@
  */
 mac_hwring_send_priv:entry
 {
-	this->mip = ((mac_client_impl_t *) args[0])->mci_mip;
-	self->out = this->mip;
-	@tot[self->out->mi_name] = count();
-	@agg = count();
+    this->mip = ((mac_client_impl_t *) args[0])->mci_mip;
+    self->out = this->mip;
+    @tot[self->out->mi_name] = count();
+    @agg = count();
 }
 
 mac_hwring_send_priv:return
 /self->out/
 {
-	if (args[1] != NULL) {
-		@full[self->out->mi_name] = count()
-	} else {
-		@ok[self->out->mi_name] = count()
-	}
-	self->out = 0;
+    if (args[1] != NULL) {
+        @full[self->out->mi_name] = count()
+    } else {
+        @ok[self->out->mi_name] = count()
+    }
+    self->out = 0;
+}
+
+ixgbe_ring_tx:entry
+{
+    this->ixgbe = ((ixgbe_tx_ring_t *)args[0])->ixgbe;
+    @rings[(uint64_t)this->ixgbe->dip, arg0] = count();
 }
 
 END
 {
-	printf("ring okay\n");
-	printa(" --- %s -> %@d\n", @ok);
+    printf("ring okay\n");
+    printa(" --- %s -> %@d\n", @ok);
 
-	printf("\nring full\n");
-	printa(" --- %s -> %@d\n", @full);
+    printf("\nring full\n");
+    printa(" --- %s -> %@d\n", @full);
 
-	printf("\ntotal (separate)\n");
-	printa(" --- %s -> %@d\n", @tot);
+    printf("\ntotal (separate)\n");
+    printa(" --- %s -> %@d\n", @tot);
 
-	printf("\ntotal (aggregate)\n");
-	printa(" --- %@d\n", @agg);
+    printf("\ntotal (aggregate)\n");
+    printa(" --- %@d\n", @agg);
+
+    printf("\nrings\n");
+    printa(" --- %x / %x -> %@d\n", @rings);
 }
